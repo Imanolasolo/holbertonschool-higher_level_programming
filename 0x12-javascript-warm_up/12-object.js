7cfe171115cb5e5864a29f17cc3ea85794ea30d1
Imanolasolo <jjusturi@gmail.com>
2022-04-04 15:03:24 -0500

    12-object.js :white_check_mark:

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// File: 12-object.js
+// By: Imanol Asolo (CodeCodix)
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
