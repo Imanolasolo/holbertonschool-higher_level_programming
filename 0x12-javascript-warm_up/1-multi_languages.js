0915f4e5d22cc0978214e4e64a0d1e4d315c170d
Imanolasolo <jjusturi@gmail.com>
2022-04-04 12:36:46 -0500

    1-multi_languages.js:white_check_mark:

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+// 1-multi_languages.js
+// By: Imanol Asolo (CodeCodix)
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
