0243d583fd88fe1bbe75edcd21efd58db4aa854d
Imanolasolo <jjusturi@gmail.com>
2022-04-04 15:27:17 -0500

    103-object_fct.js :white_check_mark:

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+//File: 103-object_fct.js
+// By: Imanol Asolo (CodeCodix)
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
