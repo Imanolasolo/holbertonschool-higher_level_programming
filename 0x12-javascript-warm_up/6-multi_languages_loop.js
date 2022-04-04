faf34fa6c66904b30b77a789430f350a65aa7530
Imanolasolo <jjusturi@gmail.com>
2022-04-04 12:51:55 -0500

    6-multi_languages_loop.js :white_check_mark:

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// 6-multi_languages_loop.js
+// By: Imanol Asolo (CodeCodix)
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
+}
