8d8b3e4f82c4d93872a5e8a3f4c2ae0cc61f56c0
Imanolasolo <jjusturi@gmail.com>
2022-04-05 11:47:28 -0500

    1-rectangle.js :white_check_mark:

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// File: 1-rectangle.js
+// By: Imanol Asolo (CodeCodix)
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
