10aa84a4c13f7cbf1d76d064a5aae99b3268d186
Imanolasolo <jjusturi@gmail.com>
2022-04-05 11:51:52 -0500

    6-square.js :white_check_mark:

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// File: 6-square.js
+// By: Imanol Asolo (CodeCodix)
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
