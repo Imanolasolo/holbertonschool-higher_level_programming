e5334947c1c574e9e88a0aee34153d076ac87e76
imanol <jjusturi@gmail.com>
2022-05-18 16:36:26 -0500

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # By Imanol Asolo.
 # Send a GET request to a given URL with a header variable.
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -X GET "$1" -H "X-School-User-Id: 98"
